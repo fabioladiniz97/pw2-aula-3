8bd4b6ba868a4d3b848c687b53d3a45c5fddbe81
Fabíola Diniz <fabioladiniz36@hotmail.com>
2023-03-23 16:41:34 -0300

    primeira atividade

---
+++ atividade.ts
@@ -0,0 +1,92 @@
+//1  Como podemos rodar isso em um arquivo .ts sem causar erros?
+
+// let employee = {};
+
+// employee.code = 10;
+
+// employee.name = "John";
+
+interface Employee {
+     code: number;
+     name: string;
+}
+
+let employee:Employee = {
+    code: 10,
+    name: "Jhon"
+
+};
+
+// 2 Como podemos melhorar o esse código usando TS?
+
+// let pessoa1 = {};
+
+// pessoa1.nome = "maria";
+
+// pessoa1.idade = 29;
+
+// pessoa1.profissao = "atriz"
+
+
+// let pessoa2 = {}
+
+// pessoa2.nome = "roberto";
+
+// pessoa2.idade = 19;
+
+// pessoa2.profissao = "Padeiro";
+
+
+
+// let pessoa3 = {
+
+// nome: "laura",
+
+// idade: "32",
+
+// profissao: "Atriz"
+
+// };
+
+
+
+// let pessoa4 = {
+
+// nome = "carlos",
+
+// idade = 19,
+
+// profissao = "padeiro"
+
+// }
+
+interface Pessoa{
+    nome: string,
+    idade: number,
+    profissao: string
+}
+
+let pessoa1:Pessoa = {
+     nome: "maria",
+     idade: 29,
+     profissao:"atriz"
+}
+
+let pessoa2:Pessoa = {
+    nome:"roberto",
+    idade:19,
+    profissao:"Padeiro"
+}
+
+let pessoa3:Pessoa = {
+    nome:"laura",
+    idade:32,
+    profissao:"Atriz"
+}
+
+let pessoa4:Pessoa = {
+    nome:"carlos",
+    idade:19,
+    profissao:"padeiro"
+
+}
